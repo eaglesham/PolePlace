999881a6e537f7a993a5ea48147e0f52ed50028d
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-02-09 18:46:55 +0000

    Admin functionality working

--- routes/admins.js
+++ routes/admins.js
@@ -6,11 +6,14 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
+    const adminID = req.params.id;
 
-    knex('options')
-      .join('votes', 'options.pollid', '=', 'votes.optionid')
+    knex('poll')
+      .join('options', 'poll.id', '=', 'options.pollid')
+      .where('creatorid', '=', adminID)
+      .join('votes', 'options.id', '=', 'votes.optionid')
       .join('voters', 'votes.optionid', '=', 'voters.id')
-      .select('title', 'description', 'points', 'name')
+      .select('polldescription', 'title', 'description', 'adminurl', 'points', 'name')
       .then((results) => {
         let templateVars = {votes: results};
         res.render("admin", templateVars);
