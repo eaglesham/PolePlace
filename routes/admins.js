35be48eaa21f23f548671b54af1f18a833de2c96
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-02-09 17:34:53 +0000

    Worked on admin pages

--- routes/admins.js
+++ routes/admins.js
@@ -7,16 +7,12 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
 
-    knex
-      .select("*")
-      .from("votes")
+    knex('options')
+      .join('votes', 'options.pollid', '=', 'votes.optionid')
+      .join('voters', 'votes.optionid', '=', 'voters.id')
+      .select('title', 'description', 'points', 'name')
       .then((results) => {
         let templateVars = {votes: results};
-        // console.log(results);
-        // [
-        // anonymous { id: 1, optionid: 1, points: 1 },
-        // anonymous { id: 2, optionid: 2, points: 2 }
-        //]
         res.render("admin", templateVars);
     });
   });
