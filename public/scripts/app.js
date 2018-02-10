b12c4acba8287d6e3d6d4b00cb0fed9400eeca61
edward eaglesham <edward.eaglesham@gmail.com>
2018-02-10 20:23:50 +0000

    starting on function for changing sortable values in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,13 @@
 $(document).ready(() => {
-  $('.sortable').sortable({
-    placeholderClass: 'list-group-item'
+  $('.sortable').sortable().bind('sortupdate', function(e, ui) {
+    console.log($('.sortable'));
+    // for (let i=0; i < thispoll.length; i++) {
+    //  $('.list-item-group')
+    // }
   });
+
 });
+
+
+// loop through your li's each time and assign new values, as was done on ejs file 
 
