dd9047a90855746e7be4c57814617ee63aacf4db
edward eaglesham <edward.eaglesham@gmail.com>
2018-02-10 23:48:07 +0000

    on click for vote button now generates an object of option:value.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,28 @@
 $(document).ready(() => {
+  
+
   $('.sortable').sortable().bind('sortupdate', function(e, ui) {
+    
     let optionsLength = $('.list-group-item').length;
     
     for (let i=0; i < $('.list-group-item').length; i++) {
-      let newValue = $('.list-group-item').length - i;
       $($('.list-group-item')[i]).attr('value', optionsLength);
       optionsLength--
     }
   });
   
+  const valueObj= {};
   const $voteButton = $('#vote-button');
-
+  
+  $voteButton.on('click', function (event) {
+    for (let i=0; i < $('.list-group-item').length; i++) {
+      let currentListItem = $($('.list-group-item')[i]).html()
+      console.log(currentListItem)
+      valueObj[currentListItem] = $($('.list-group-item')[i]).attr('value');
+    }
+    console.log(valueObj);
+  })
+  
 
 });
 
