bcdbf87263f61be4830256a37a18217db175309b
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-02-11 21:32:06 +0000

    Basic knex query working again

--- routes/admins.js
+++ routes/admins.js
@@ -7,15 +7,18 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
     const adminID = req.params.id;
-
-    knex('poll')
+      console.log(adminID)
+      knex('poll')
       .join('options', 'poll.id', '=', 'options.pollid')
-      .where('adminurl', '=', adminID)
+      .where('adminurl', adminID)
       .join('votes', 'options.id', '=', 'votes.optionid')
-      .join('voters', 'votes.optionid', '=', 'voters.id')
-      .select('polldescription', 'title', 'description', 'adminurl', 'points', 'name')
+      // .join('voters', 'votes.optionid', '=', 'voters.id')
+      // .sum('points as total')
+      // .groupBy('title')
+      .select('polldescription', 'title', 'description', 'adminurl', 'optionid', 'points')
       .then((results) => {
         let templateVars = {votes: results};
+        console.log('THESE ARE THE RESULTS', results);
         res.render("admin", templateVars);
     });
   });
