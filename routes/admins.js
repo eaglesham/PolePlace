a754c435f40c53b27a40d3a7526057ae719d0e21
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-02-12 00:06:48 +0000

    Working on summation query

--- routes/admins.js
+++ routes/admins.js
@@ -7,18 +7,15 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
     const adminID = req.params.id;
-      console.log(adminID)
-      knex('poll')
-      .join('options', 'poll.id', '=', 'options.pollid')
-      .where('adminurl', adminID)
-      .join('votes', 'options.id', '=', 'votes.optionid')
+    knex('poll')
+      .join('options', 'poll.id', 'options.pollid')
+      .where('adminurl',adminID)
+      .join('votes', 'options.id', 'votes.optionid')
       // .join('voters', 'votes.optionid', '=', 'voters.id')
-      // .sum('points as total')
-      // .groupBy('title')
       .select('polldescription', 'title', 'description', 'adminurl', 'optionid', 'points')
       .then((results) => {
         let templateVars = {votes: results};
-        console.log('THESE ARE THE RESULTS', results);
+        console.log(results);
         res.render("admin", templateVars);
     });
   });
